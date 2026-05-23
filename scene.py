48bf5ca44e7ec86e2277253e15397acbc3174ca7
Naman Chhabra <chhabranaman21@gmail.com>
2026-05-23 18:08:20 +0530

    `scene.py` for Scenes used in `main.py`
    - This file contains Starting and Ending Game scenes.
    - Plays video and audio to create a Scene using module `splashscreen-engine`

---
+++ scene.py
@@ -0,0 +1,111 @@
+import random
+
+import pygame
+import sys
+import splashscreen_engine as splash # For Video Playing | pip install splashscreen-engine
+
+
+pygame.init()
+loadimg = pygame.image.load
+pygame.display.set_caption("My Story")
+pygame.mixer.init()
+
+is_mute = False
+class GameOverScene:
+    def __init__(self, width, height,mute=is_mute):
+        global is_mute
+        window= splash.Screen()
+        window.size(width,height)
+        window.start()
+        window.title("GameOver")
+
+        skip_text = splash.Text(window,"Press `Space` to Skip.","lucida handwriting",30,(width*0.27,height*0.9))
+        skip_text.show()
+        bg_vid = splash.BackgroundVideo(window,"assets/Scenes/gameOverVid.mp4",fps=60)
+        bg_vid.play()
+
+        pygame.display.set_mode((width, height))
+
+        window.wait(0.5)
+        sound1 = pygame.mixer.Channel(0)
+        sound1.set_volume(0)
+        if not mute:
+            sound1.set_volume(0.2)
+        gameOverSound = pygame.mixer.Sound("assets/Scenes/gameOverSound.mp3")
+        sound1.play(gameOverSound)
+        while bg_vid.is_playing:
+
+            for event in pygame.event.get():
+                # Handle window close
+                if event.type == pygame.QUIT:
+                    sound1.stop()
+                    window.stop()
+                    sys.exit()  # Safely exits the entire Python script
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+
+                        if mute:
+                            mute = False
+                            sound1.set_volume(0.2)
+                        else:
+                            mute = True
+                            sound1.set_volume(0)
+                        is_mute = mute
+                    if event.key == pygame.K_SPACE:
+                        sound1.stop()
+                        window.stop(quit_pygame=False)
+                        pygame.display.update()
+                        return
+
+        window.stop(quit_pygame=False)
+
+
+class GameStartScene:
+    def __init__(self, width, height,mute=is_mute):
+        global is_mute
+        window= splash.Screen()
+        window.size(width,height)
+        window.start()
+        window.title("Rocket Game")
+
+        bg_vid = splash.BackgroundVideo(window,"assets/Scenes/gameStartVid.mp4",fps=60)
+        bg_vid.play()
+        skip_text = splash.Text(window, "Press `Space` to Skip.", "lucida handwriting", 30,
+                                (width * 0.27, height * 0.9))
+        skip_text.show()
+        pygame.display.set_mode((width, height))
+
+        # window.wait(1)
+        sound1 = pygame.mixer.Channel(0)
+        sound1.set_volume(0)
+        if not mute:
+            sound1.set_volume(100)
+        gameStartSound = pygame.mixer.Sound("assets/Scenes/gameStartSound.mp3")
+        sound1.play(gameStartSound)
+        while bg_vid.is_playing:
+
+            for event in pygame.event.get():
+                # Handle window close
+                if event.type == pygame.QUIT:
+                    sound1.stop()
+                    window.stop()
+                    sys.exit()  # Safely exits the entire Python script
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+
+                        if mute:
+                            mute = False
+                            sound1.set_volume(100)
+                        else:
+                            mute = True
+                            sound1.set_volume(0)
+                        is_mute = mute
+                    if event.key == pygame.K_SPACE:
+                        sound1.stop()
+                        window.stop(quit_pygame=False)
+                        pygame.display.update()
+                        return
+        sound1.fadeout(3)
+
+        window.stop(quit_pygame=False)
+
